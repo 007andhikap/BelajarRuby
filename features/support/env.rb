552c42db2c9f9cc48bd3fd17d73f7e7c7d78e215
007andhikap <S@ngperusuh123>
2025-03-12 14:09:09 +0700

    Fix the github-action config and setup (final)

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,11 +10,14 @@ options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless') unless ENV['CI'].nil? # Selalu headless di CI/CD
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox') # Tambahkan opsi no-sandbox agar tidak crash
+options.add_argument('--no-sandbox') # Hindari crash
 options.add_argument('--disable-popup-blocking')
 options.add_argument('--disable-infobars')
 options.add_argument('--remote-debugging-port=9222')
-options.add_argument('--user-data-dir=/tmp/chrome-user-data') # Pastikan sesi unik
+options.add_argument("--user-data-dir=/tmp/chrome-user-data-$$") # Direktori unik
+options.add_argument('--disable-software-rasterizer') # Hindari crash rendering
+options.add_argument('--disable-background-timer-throttling') # Pastikan eksekusi JS lancar
+options.add_argument('--disable-extensions') # Mematikan ekstensi agar lebih stabil
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app,browser: :chrome, timeout: 30, options: options)
