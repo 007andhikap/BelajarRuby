d589661baaf347db97b891c0e29cdc141aadc7b1
007andhikap <S@ngperusuh123>
2025-03-12 13:58:06 +0700

    Fix the github-action config and setup

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,16 +2,21 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'cucumber'
 require 'rspec'
-require 'webdrivers'
+require 'selenium-webdriver'
 require 'pry'
 
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless') if ENV['HEADLESS']
+# Pastikan mode headless aktif di CI/CD
+options.add_argument('--headless') unless ENV['CI'].nil? # Selalu headless di CI/CD
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox') # Tambahkan opsi no-sandbox agar tidak crash
+options.add_argument('--disable-popup-blocking')
+options.add_argument('--disable-infobars')
+options.add_argument('--remote-debugging-port=9222')
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome, timeout: 30, options: options)
+  Capybara::Selenium::Driver.new(app,browser: :chrome, timeout: 30, options: options)
 end
 
 Capybara.configure do |config|
