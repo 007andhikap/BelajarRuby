f93f15201a5d6dddfd90f2f88cc952131156d6bc
007andhikap <S@ngperusuh123>
2025-03-14 19:19:01 +0700

    Menyimpan perubahan sebelum rebase

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,10 +9,10 @@ options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless') if ENV['HEADLESS']
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
-options.add_argument('--user-data-dir=/tmp/chrome_profile') # Use a unique directory
 options.add_argument('--no-sandbox')
+options.add_argument("--user-data-dir=#{Dir.pwd}/tmp/chrome_user_data") # Direktori unik
 
-Capybara.register_driver :selenium do |app|
+Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, timeout: 30, options: options)
 end
 
